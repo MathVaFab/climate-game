c847e5453fc57feb71c3220f87bcfaff6523fc18
MathVaFab <mathieu.valloire@fabernovel.com>
2023-09-01 18:31:41 +0200

    Overlay et character
    Ajout d'un overlay et de character.
    
    supprimerBackground()
    Permet de supprimer l'overlay quand l'utilisateur clique sur le bouton Continue
    
    afficherTexte(texte)
    Permet de gérer l'affichage par caractère et l'affichage du bouton. Est appelé dans landbot par window.afficherTexte("mon texte")
    
    ajouterBanner()
    Permet d'ajouter la bannière. Est appelé par Landbot avec window.ajouterBanner()

--- public/scripts/img.js
+++ public/scripts/img.js
@@ -19,6 +19,7 @@ function createImage(varName, status) {
     img.alt = varName;
     img.width = '400';
     img.style.position = "absolute";
+    img.classList.add('background-game');
 
     // Utiliser le zIndex de la variable passée plutôt que d'une valeur fixe
     img.style.zIndex = dataIndex.get(varName) || 0;  // Si varName n'existe pas dans dataIndex, utiliser 0 comme valeur par défaut
@@ -39,3 +40,59 @@ function deleteId(id) {
     console.warn(`Élément avec l'ID ${id} non trouvé.`);
 }
 }
+
+function ajouterBanner() {
+  // Le code HTML à ajouter
+  var html = `
+    <div class="background">
+      <div class="banner">
+        <img class="character" src="./images/character/speaking.png" alt="Image de bannière">
+        <div class="text">
+          <div class="bubble grow left"></div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  // Trouver la balise <body> et y ajouter le code HTML
+  document.body.insertAdjacentHTML('beforeend', html);
+}
+
+
+function afficherTexte(texte) {
+  // Le texte que vous voulez afficher
+  var monTexte = texte; // Texte passé en paramètre
+  var i = 0;
+
+  // Notez ici que nous utilisons getElementsByClassName et sélectionnons le premier élément
+  var elements = document.getElementsByClassName("bubble grow left");
+  var element;
+  if (elements.length > 0) {
+    element = elements[0];
+
+    // Utilisation de setInterval pour ajouter un caractère toutes les 100 millisecondes
+    var interval = setInterval(function() {
+      if (i < monTexte.length) {
+        element.innerHTML += monTexte.charAt(i);
+        i++;
+      } else {
+        clearInterval(interval); // Arrête le setInterval quand tout le texte est affiché
+        element.insertAdjacentHTML('beforeend', '<a href="javascript:void(0)" onclick="supprimerBackground()" class="eightbit-btn">Continue</a>');
+      }
+    }, 100); // 100 millisecondes entre chaque caractère
+  } else {
+    console.log("Aucun élément avec la classe 'bubble grow left' n'a été trouvé.");
+  }
+}
+
+function supprimerBackground() {
+  // Rechercher tous les éléments avec la classe 'background'
+  var elements = document.getElementsByClassName("background");
+
+  // Parcourir le tableau et supprimer chaque élément
+  while (elements.length > 0) {
+    elements[0].remove();
+  }
+}
+
+

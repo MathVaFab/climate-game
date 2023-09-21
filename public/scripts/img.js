29efb705abfe9e125ba511f6668a8d284dd943ec
Arthur Galliot <galliot.arthur@gmail.com>
2023-09-21 18:58:46 +0200

    [FIX-???] Fix JS and write responsive design

--- public/scripts/img.js
+++ public/scripts/img.js
@@ -1,34 +1,31 @@
 // Utiliser une convention de nommage cohérente pour les variables et les fonctions
 const dataIndex = new Map();
-dataIndex.set('facadesblanches', 10);
-dataIndex.set('traffic', 20);
-dataIndex.set('trocacouvert', 25);
-dataIndex.set('parisousbois', 30);
-dataIndex.set('haussmansousbois', 40);
-dataIndex.set('popdensite', 100);
-dataIndex.set('charbon', 1000);
-dataIndex.set('pub',21);
+dataIndex.set("facadesblanches", 10);
+dataIndex.set("traffic", 20);
+dataIndex.set("trocacouvert", 25);
+dataIndex.set("parisousbois", 30);
+dataIndex.set("haussmansousbois", 40);
+dataIndex.set("popdensite", 100);
+dataIndex.set("charbon", 1000);
+dataIndex.set("pub", 21);
 
 function createImage(varName, status) {
-    const container = document.getElementById("chartjs-container");
+  const container = document.getElementById("chartjs-container");
 
-    // Créer un nouvel élément img
-    const img = document.createElement("img");
-    alert("ok");
+  // Créer un nouvel élément img
+  const img = document.createElement("img");
 
-    // Définir les attributs pour l'élément img
-    img.src = `images/game/${varName}.png`;
-    img.alt = varName;
-    img.width = '400';
-    img.style.position = "absolute";
-    img.classList.add('background-game');
+  // Définir les attributs pour l'élément img
+  img.src = `images/game/${varName}.png`;
+  img.alt = varName;
+  img.classList.add("background-game");
+  console.log(varName, status);
+  // Utiliser le zIndex de la variable passée plutôt que d'une valeur fixe
+  img.style.zIndex = status ?? 0; // Si varName n'existe pas dans dataIndex, utiliser 0 comme valeur par défaut
+  img.id = varName;
 
-    // Utiliser le zIndex de la variable passée plutôt que d'une valeur fixe
-    img.style.zIndex = ${status} || 0;  // Si varName n'existe pas dans dataIndex, utiliser 0 comme valeur par défaut
-    img.id = varName;
-
-    // Ajouter l'élément img dans le div
-    container.appendChild(img);
+  // Ajouter l'élément img dans le div
+  container.appendChild(img);
 }
 
 function deleteId(id) {
@@ -38,9 +35,9 @@ function deleteId(id) {
   // Supprimer l'élément
   if (element) {
     element.remove();
-} else {
+  } else {
     console.warn(`Élément avec l'ID ${id} non trouvé.`);
-}
+  }
 }
 
 function ajouterBanner() {
@@ -57,9 +54,8 @@ function ajouterBanner() {
   `;
 
   // Trouver la balise <body> et y ajouter le code HTML
-  document.body.insertAdjacentHTML('beforeend', html);
+  document.body.insertAdjacentHTML("beforeend", html);
 }
-
 
 function afficherTexte(texte) {
   // Le texte que vous voulez afficher
@@ -73,17 +69,22 @@ function afficherTexte(texte) {
     element = elements[0];
 
     // Utilisation de setInterval pour ajouter un caractère toutes les 100 millisecondes
-    var interval = setInterval(function() {
+    var interval = setInterval(function () {
       if (i < monTexte.length) {
         element.innerHTML += monTexte.charAt(i);
         i++;
       } else {
         clearInterval(interval); // Arrête le setInterval quand tout le texte est affiché
-        element.insertAdjacentHTML('beforeend', '<a href="javascript:void(0)" onclick="supprimerBackground()" class="eightbit-btn">Continue</a>');
+        element.insertAdjacentHTML(
+          "beforeend",
+          '<a href="javascript:void(0)" onclick="supprimerBackground()" class="eightbit-btn">Continue</a>'
+        );
       }
     }, 100); // 100 millisecondes entre chaque caractère
   } else {
-    console.log("Aucun élément avec la classe 'bubble grow left' n'a été trouvé.");
+    console.log(
+      "Aucun élément avec la classe 'bubble grow left' n'a été trouvé."
+    );
   }
 }
 
@@ -96,5 +97,3 @@ function supprimerBackground() {
     elements[0].remove();
   }
 }
-
-
